2c5844528a1a775c1c28cd717ee4f8c4c86dcdbe
Kyle Rinoza <isti@unmg.com.ph>
2025-07-21 00:03:20 +0800

    add stacking cards for memberships

--- src/routes/public/Home/membership.tsx
+++ src/routes/public/Home/membership.tsx
@@ -1,15 +1,17 @@
+import StackedDragCarousel from "@/components/routes/memberships/cards";
 import { Button } from "@/components/ui/button";
 
 function MembershipPlans() {
   return (
     <section className="min-h-[100vh] flex flex-col gap-4 p-8">
-      <header className="text-center font-menbere font-bold text-3xl uppercase">
+      <header className="text-center font-lexend font-bold text-3xl uppercase">
         Become a member
       </header>
       <p className="text-sm text-center">
         Choose your grind — from full access to student-friendly rates, we've
         got a membership plan that fits your goals and your budget!
       </p>
+      <StackedDragCarousel />
       <div className="w-full flex flex-col gap-8 items-center">
         <div className="relative w-full max-w-[300px] min-h-[200px] flex flex-col gap-4 justify-between font-lexend text-white bg-black [clip-path:polygon(0_0,calc(100%_-_1rem)_0,100%_1rem,100%_100%,1rem_100%,0_calc(100%_-_1rem))] p-4">
           <header className="font-audiowide text-xl">
